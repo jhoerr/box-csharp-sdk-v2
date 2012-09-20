42a03fdb33c3d7f5599404f8215897f44d9bfa0d
John Hoerr <jhoerr@gmail.com>
2012-09-20 19:26:57 -0400

    Removing deprecated code

--- SDK/EventHandlers.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BoxApi.V2
-{
-    /// <summary>
-	/// Represents the callback method to invoke when operation is finished
-	/// </summary>
-	/// <typeparam name="TResponseType">Type of operation status</typeparam>
-	/// <param name="response">Response information</param>
-	public delegate void OperationFinished<TResponseType>(TResponseType response);
-}
