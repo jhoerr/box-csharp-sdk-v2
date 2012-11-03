d51e20e293d097adfbd731575802ff7d0760f2cf
John Hoerr <jhoerr@gmail.com>
2012-11-02 22:28:28 -0400

    Can now save api key & token for unit testing

--- BoxApi.V2.Samples.WebAuthentication.MVC/Controllers/HomeController.cs
+++ BoxApi.V2.Samples.WebAuthentication.MVC/Controllers/HomeController.cs
@@ -1,17 +1,34 @@
-﻿using System.Web.Mvc;
+﻿using System.Text;
+using System.Web.Mvc;
 using BoxApi.V2.Model;
 
 namespace BoxApi.V2.Samples.WebAuthentication.MVC.Controllers
 {
     public class HomeController : Controller
     {
+        private const string JsonFormat =
+            @"{{
+    ""AppKey"": ""{0}"",
+    ""AuthKey"": ""{1}"",
+    ""TestEmail"": ""box.csharp.sdk@gmail.com"",
+    ""CollaboratingUser"": ""186800768""
+}}";
+
         public ActionResult Index()
         {
+            ClearSession();
             return View();
+        }
+
+        private void ClearSession()
+        {
+            Session["ApiKey"] = null;
+            Session["Token"] = null;
         }
 
         public ActionResult InitiateAuthorization(string apiKey)
         {
+            Session["ApiKey"] = apiKey;
             var boxAuthenticator = new BoxAuthenticator(apiKey);
             var authorizationUrl = boxAuthenticator.GetAuthorizationUrl();
             return new RedirectResult(authorizationUrl);
@@ -19,8 +36,17 @@ namespace BoxApi.V2.Samples.WebAuthentication.MVC.Controllers
 
         public ActionResult Authorize(string ticket, string auth_token)
         {
-            var model = new BoxAuthToken() {AuthToken = auth_token};
+            Session["Token"] = auth_token;
+            var model = new BoxAuthToken {AuthToken = auth_token};
             return View(model);
+        }
+
+        public FileResult Save()
+        {
+            var apiKey = Session["ApiKey"] as string;
+            var token = Session["Token"] as string;
+            var result = string.Format(JsonFormat, apiKey, token);
+            return File(Encoding.UTF8.GetBytes(result), "application/json", "test_info.json");
         }
     }
 }
