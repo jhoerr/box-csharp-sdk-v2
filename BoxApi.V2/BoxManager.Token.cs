9a2759a8492153301615b8c322a43d33139f4aaa
John Hoerr <jhoerr@gmail.com>
2012-11-09 16:27:11 -0500

    Documented token methods

--- BoxApi.V2/BoxManager.Token.cs
+++ BoxApi.V2/BoxManager.Token.cs
@@ -9,12 +9,23 @@ namespace BoxApi.V2
 {
     public partial class BoxManager
     {
+        /// <summary>
+        /// Create a sandboxed authentication token and corresponding folder for a user.  The user does not need to previously have a registered Box account.
+        /// </summary>
+        /// <param name="emailAddress">The sanboxed user's email address</param>
+        /// <returns>A sandboxed authentication token</returns>
         public BoxToken CreateToken(string emailAddress)
         {
             IRestRequest request = _requestHelper.CreateToken(emailAddress);
             return _restClient.ExecuteAndDeserialize<BoxToken>(request);
         }
 
+        /// <summary>
+        /// Create a sandboxed authentication token and corresponding folder for a user.  The user does not need to previously have a registered Box account.
+        /// </summary>
+        /// <param name="onSuccess">Action to perform with the sandboxed authentication token</param>
+        /// <param name="onFailure">Action to perform following a failed Token operation</param>
+        /// <param name="emailAddress">The user's email address</param>
         public void CreateToken(Action<BoxToken> onSuccess, Action<Error> onFailure, string emailAddress)
         {
             IRestRequest request = _requestHelper.CreateToken(emailAddress);
