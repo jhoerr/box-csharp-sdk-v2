65d51c30086e9058d366ca103e1dc601aea0299d
John Hoerr <jhoerr@gmail.com>
2013-01-26 06:49:55 -0500

    Adding SearchResultCollection to model

---
+++ BoxApi.V2/Model/SearchResultCollection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BoxApi.V2.Model
+{
+    /// <summary>
+    /// The search results of a user's Box
+    /// </summary>
+    public class SearchResultCollection : Collection<Entity>
+    {
+        /// <summary>
+        /// The search result at which the response was started
+        /// </summary>
+        public int Offset { get; set; }
+    }
+}
