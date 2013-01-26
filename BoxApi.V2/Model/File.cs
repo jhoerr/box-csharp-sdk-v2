e3965856de3a334c8f9af3a88e27482bd90bcf95
John Hoerr <jhoerr@gmail.com>
2013-01-26 04:46:32 -0500

    Removing deprecated Path & PathId

--- BoxApi.V2/Model/File.cs
+++ BoxApi.V2/Model/File.cs
@@ -8,17 +8,6 @@ namespace BoxApi.V2.Model
     public class File : HierarchyEntity
     {
         /// <summary>
-        ///     The path from the user’s root to this file by folder names
-        /// </summary>
-        public string Path { get; set; }
-
-        /// <summary>
-        ///     The path from the root to this file by folder ids
-        /// </summary>
-        [JsonProperty(PropertyName = "path_id")]
-        public string PathId { get; set; }
-
-        /// <summary>
         ///     The sha1 hash of the file.  Useful for quickly determining if the contents of the file have changed.
         /// </summary>
         public string Etag { get; set; }
