67d836345195ee2791c474d47765d4b0ce010982
John Hoerr <jhoerr@gmail.com>
2013-01-27 12:56:30 -0500

    Changed size to double (from long)
    Box returns large integer values as doubles.  This should future-proof very very large folders.

--- BoxApi.V2/Model/Version.cs
+++ BoxApi.V2/Model/Version.cs
@@ -5,7 +5,7 @@ namespace BoxApi.V2.Model
         /// <summary>
         ///     The item size in bytes
         /// </summary>
-        public long Size { get; set; }
+        public double Size { get; set; }
     }
 
     public class VersionCollection : Collection<Version>
