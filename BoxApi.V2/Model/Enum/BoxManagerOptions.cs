e94934c41e8bfc2a516576d74b221c33013adb5a
John Hoerr <jhoerr@gmail.com>
2013-02-05 13:00:28 -0500

    Adding BoxManagerOptions enum
    These are options to customize the behavior of the BoxManager.  The only
    option curently is to 'RetryRequestWhenHttp500Received'.  This is added
    to try to smooth out the client behavior a bit when things go wrong with
    the request for server-side reasons.

---
+++ BoxApi.V2/Model/Enum/BoxManagerOptions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BoxApi.V2.Model.Enum
+{
+    /// <summary>
+    /// Options for customizing the behavior of the BoxManager.
+    /// </summary>
+    [Flags]
+    public enum BoxManagerOptions
+    {
+        /// <summary>
+        /// No options requested
+        /// </summary>
+        None = 0,
+        /// <summary>
+        /// Any request that receives an HTTP 500 (Internal Server Error) response should be retried once.
+        /// </summary>
+        RetryRequestWhenHttp500Received,
+    }
+}
