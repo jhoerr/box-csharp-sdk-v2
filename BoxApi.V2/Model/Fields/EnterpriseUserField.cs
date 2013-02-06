e5dbbc83c2d93b654933930b2e22e0f540d573a0
John Hoerr <jhoerr@gmail.com>
2013-02-06 13:24:25 -0500

    Changed *UserField inheritence

--- BoxApi.V2/Model/Fields/EnterpriseUserField.cs
+++ BoxApi.V2/Model/Fields/EnterpriseUserField.cs
@@ -2,13 +2,22 @@
 {
     public class EnterpriseUserField : UserField
     {
-        public static Field Role = new Field("role");
-        public static Field TrackingCodes = new Field("tracking_codes");
-        public static Field CanSeeManagedUsers = new Field("can_see_managed_users");
-        public static Field IsSyncEnabled = new Field("is_sync_enabled");
-        public static Field IsExemptFromDeviceLimits = new Field("is_exempt_from_device_limits");
-        public static Field IsExemptFromLoginVerification = new Field("is_exempt_from_login_verification");
-        public static Field Enterprise = new Field("enterprise");
-            
+        public static UserField Role = new UserField("role");
+        public static UserField TrackingCodes = new UserField("tracking_codes");
+        public static UserField CanSeeManagedUsers = new UserField("can_see_managed_users");
+        public static UserField IsSyncEnabled = new UserField("is_sync_enabled");
+        public static UserField IsExemptFromDeviceLimits = new UserField("is_exempt_from_device_limits");
+        public static UserField IsExemptFromLoginVerification = new UserField("is_exempt_from_login_verification");
+        public static UserField Enterprise = new UserField("enterprise");
+
+        public static UserField[] All = new UserField[]
+            {
+                Address, AvatarUrl, CreatedAt, JobTitle, Language, Login, MaxUploadSize, ModifiedAt, Name, Phone, SpaceAmount, SpaceUsed, Status,
+                Role, TrackingCodes, CanSeeManagedUsers, IsSyncEnabled, IsExemptFromDeviceLimits, IsExemptFromLoginVerification, Enterprise
+            };
+
+        public EnterpriseUserField(string value) : base(value)
+        {
+        }
     }
 }
--- BoxApi.V2/Model/Fields/UserField.cs
+++ BoxApi.V2/Model/Fields/UserField.cs
@@ -1,19 +1,23 @@
 ﻿namespace BoxApi.V2.Model.Fields
 {
-    public class UserField
+    public class UserField : Field
     {
-        public static Field Address = new Field("address");
-        public static Field AvatarUrl = new Field("avatar_url");
-        public static Field CreatedAt = new Field("created_at");
-        public static Field JobTitle = new Field("job_title");
-        public static Field Language = new Field("language");
-        public static Field Login = new Field("login");
-        public static Field MaxUploadSize = new Field("max_upload_size");
-        public static Field ModifiedAt = new Field("modified_at");
-        public static Field Name = new Field("name");
-        public static Field Phone = new Field("phone");
-        public static Field SpaceAmount = new Field("space_amount");
-        public static Field SpaceUsed = new Field("space_used");
-        public static Field Status = new Field("status");
+        public static UserField Address = new UserField("address");
+        public static UserField AvatarUrl = new UserField("avatar_url");
+        public static UserField CreatedAt = new UserField("created_at");
+        public static UserField JobTitle = new UserField("job_title");
+        public static UserField Language = new UserField("language");
+        public static UserField Login = new UserField("login");
+        public static UserField MaxUploadSize = new UserField("max_upload_size");
+        public static UserField ModifiedAt = new UserField("modified_at");
+        public static UserField Name = new UserField("name");
+        public static UserField Phone = new UserField("phone");
+        public static UserField SpaceAmount = new UserField("space_amount");
+        public static UserField SpaceUsed = new UserField("space_used");
+        public static UserField Status = new UserField("status");
+
+        public UserField(string value) : base(value)
+        {
+        }
     }
 }
