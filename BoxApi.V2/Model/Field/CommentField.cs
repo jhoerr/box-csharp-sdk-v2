0103542a5ce5cec7a5c99362c4e169d5a0a7acaa
John Hoerr <jhoerr@gmail.com>
2013-02-06 12:59:47 -0500

    Adding CommentField

---
+++ BoxApi.V2/Model/Field/CommentField.cs
@@ -0,0 +1,9 @@
+﻿namespace BoxApi.V2.Model.Field
+{
+    public class CommentField : TemporalField
+    {
+        public static Field IsReplyComment = new Field("is_reply_comment");
+        public static Field Message = new Field("message");
+        public static Field Item = new Field("item");
+    }
+}
