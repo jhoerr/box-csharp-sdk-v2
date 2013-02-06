7867d1f8dcb08c61fd892fcb688d61e14c04bc81
John Hoerr <jhoerr@gmail.com>
2013-02-06 13:00:44 -0500

    Adding Discussion Field

---
+++ BoxApi.V2/Model/Field/DiscussionField.cs
@@ -0,0 +1,9 @@
+﻿namespace BoxApi.V2.Model.Field
+{
+    public class DiscussionField : TemporalField
+    {
+        public static Field Name = new Field("name");
+        public static Field Parent = new Field("parent");
+        public static Field Description = new Field("description");
+    }
+}
