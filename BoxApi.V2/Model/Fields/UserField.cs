66a4dce836d28b86b96334a7561624318d302f36
John Hoerr <jhoerr@gmail.com>
2013-02-06 13:14:20 -0500

    Adding UserField

---
+++ BoxApi.V2/Model/Fields/UserField.cs
@@ -0,0 +1,19 @@
+﻿namespace BoxApi.V2.Model.Fields
+{
+    public class UserField
+    {
+        public static Field Address = new Field("address");
+        public static Field AvatarUrl = new Field("avatar_url");
+        public static Field CreatedAt = new Field("created_at");
+        public static Field JobTitle = new Field("job_title");
+        public static Field Language = new Field("language");
+        public static Field Login = new Field("login");
+        public static Field MaxUploadSize = new Field("max_upload_size");
+        public static Field ModifiedAt = new Field("modified_at");
+        public static Field Name = new Field("name");
+        public static Field Phone = new Field("phone");
+        public static Field SpaceAmount = new Field("space_amount");
+        public static Field SpaceUsed = new Field("space_used");
+        public static Field Status = new Field("status");
+    }
+}
