e3b1e4b896ca7996fae3eef2668ab3ca2ce57998
John Hoerr <jhoerr@gmail.com>
2013-02-27 12:49:49 -0500

    Extension method to count all items in a folder
    Added an extension method that will count the total number of items
    contained within a folder and all of its subfolders.

---
+++ BoxApi.V2.Tests/Extensions/BoxManager.Folder.ExtensionsTests.cs
@@ -0,0 +1,45 @@
+﻿using BoxApi.V2.Extensions;
+using BoxApi.V2.Model;
+using BoxApi.V2.Tests.Harness;
+using NUnit.Framework;
+
+namespace BoxApi.V2.Tests.Extensions
+{
+    [TestFixture]
+    internal class BoxManagerFolderExtensionsTests : BoxApiTestHarness
+    {
+        [Test]
+        public void GetTotalItemsCount()
+        {
+            /*
+             * testFolder/
+             *   file1
+             *   testSubFolder1/
+             *     file2
+             *   testSubFolder2/
+             *     file3
+             *     testSubSubFolder/
+             *       file4
+             *       file5
+             */
+            Folder testFolder = Client.CreateFolder("0", TestItemName());
+            try
+            {
+                Folder testSubFolder1 = Client.CreateFolder(testFolder.Id, TestItemName());
+                Folder testSubFolder2 = Client.CreateFolder(testFolder.Id, TestItemName());
+                Folder testSubSubFolder = Client.CreateFolder(testSubFolder2.Id, TestItemName());
+                Client.CreateFile(testFolder.Id, TestItemName());
+                Client.CreateFile(testSubFolder1.Id, TestItemName());
+                Client.CreateFile(testSubFolder2.Id, TestItemName());
+                Client.CreateFile(testSubSubFolder.Id, TestItemName());
+                Client.CreateFile(testSubSubFolder.Id, TestItemName());
+                long totalItemsInFolder = Client.TotalItemsInFolder(testFolder.Id);
+                Assert.That(totalItemsInFolder, Is.EqualTo(8));
+            }
+            finally
+            {
+                Client.Delete(testFolder, true);
+            }
+        }
+    }
+}
---
+++ BoxApi.V2/Extensions/BoxManager.Folder.Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using BoxApi.V2.Model;
+using BoxApi.V2.Model.Fields;
+
+namespace BoxApi.V2.Extensions
+{
+    public static class BoxManagerFolderExtensions
+    {
+        /// <summary>
+        /// Returns the total number of items in this folder and all folders contained therein.  A folder is considered as a distinct item.  If folder 'A' contains one file and one subfolder, it contains two items.
+        /// </summary>
+        /// <param name="boxManager">The BoxManager with which to perform the count.</param>
+        /// <param name="folderId">The ID of the folder to count.</param>
+        /// <returns>The total number of items in this folder and all folders contained therein</returns>
+        public static long TotalItemsInFolder(this BoxManager boxManager, string folderId)
+        {
+            Folder folder = boxManager.GetFolder(folderId, new[] {FolderField.ItemCollection});
+            return folder.ItemCollection.TotalCount + folder.Folders.Sum(f => TotalItemsInFolder(boxManager, f.Id));
+        }
+    }
+}
